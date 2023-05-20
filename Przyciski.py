8067129da6dbe47fae67a2cbf559479edb1d2460
HeydukW <heyduk.weronika@gmail.com>
2023-05-20 22:00:01 +0200

    Add station list with ID and location #2

--- Przyciski.py
+++ Przyciski.py
@@ -84,7 +84,44 @@ def get_measurement(station_id):
 
 
 def check_air_quality_index():
-    station_id = input("Podaj ID stacji: ")
+    def generate_station_list():
+        all_station_data = download_data(URL_STATION)
+        all_station = conv_data_to_json(all_station_data)
+        print("Lista ID stacji:")
+        for station in all_station:
+            station_coords = (station['gegrLat'], station['gegrLon'])
+            station_location = get_location_from_coordinates(station_coords)
+            print(f"ID: {station['id']}, Lokalizacja: {station_location}")
+
+    def get_location_from_coordinates(coords):
+        geolocator = Nominatim(user_agent="air_quality_app")
+        location_data = geolocator.reverse(coords)
+        if location_data:
+            return location_data.address
+        else:
+            return "Nieznana lokalizacja"
+
+    root = tk.Tk()
+
+    frame = tk.Frame(root)
+    frame.pack()
+
+    station_list_button = tk.Button(frame, text="Generuj listę ID stacji", command=generate_station_list)
+    station_list_button.pack(side=tk.LEFT)
+
+    station_id_label = tk.Label(frame, text="ID stacji:")
+    station_id_label.pack(side=tk.LEFT)
+
+    station_id_entry = tk.Entry(frame)
+    station_id_entry.pack(side=tk.LEFT)
+
+    check_index_button = tk.Button(frame, text="Sprawdź indeks jakości powietrza", command=lambda: check_index(station_id_entry.get()))
+    check_index_button.pack(side=tk.LEFT)
+
+    root.mainloop()
+
+
+def check_index(station_id):
     air_quality_index_data = download_data(URL_AIR_QUALITY_INDEX, station_id)
     air_quality_index = conv_data_to_json(air_quality_index_data)
 
@@ -94,6 +131,9 @@ def check_air_quality_index():
         print(f"Data: {air_quality_index['stIndexLevel']['calcDate']}")
     else:
         print("Nieprawidłowe dane indeksu jakości powietrza.")
+
+
+
 
 
 def find_nearest_station():
@@ -161,4 +201,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
